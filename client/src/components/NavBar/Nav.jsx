84e3ea9910cf30d5d4d90ba99084ffbefc9ed3f4
Clifford-Joshua <155918426+Clifford-Joshua@users.noreply.github.com>
2025-09-25 17:47:05 +0100

    nav bar height issue

--- client/src/components/NavBar/Nav.jsx
+++ client/src/components/NavBar/Nav.jsx
@@ -4,7 +4,7 @@ import { Title, SideBarBtn, IntroMessage } from "./components";
 
 const Nav = () => {
   return (
-    <Wrapper className="bg-white h-[10vh] md:h-[9vh]">
+    <Wrapper className="bg-white h-[10vh] md:h-[9vh] lg:h-[12vh]">
       <nav className="h-[100%]">
         <div className="flex md:justify-between items-center p-[1rem] h-[100%]">
           <SideBarBtn />
